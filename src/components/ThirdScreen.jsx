c13e5b51c27adec7c19c42b294a382321422c78c
jaffar-ux <hawlaadmi@gmail.com>
2025-07-30 11:27:23 +0530

    update components ThirdScreen.jsx

--- src/components/ThirdScreen.jsx
+++ src/components/ThirdScreen.jsx
@@ -10,10 +10,10 @@ import "swiper/css/navigation"
 
 export default function ThirdScreen({ onNext }) {
   const photos = [
-    { src: "/images/1.jpg", alt: "Memory 1" },
-    { src: "/images/2.jpg", alt: "Memory 2" },
-    { src: "/images/3.jpg", alt: "Memory 3" },
-    { src: "/images/4.jpg", alt: "Memory 4" },
+    { src: "/images/001.jpg", alt: "Myphoto" },
+    { src: "/images/002.jpg", alt: "Myphoto" },
+    { src: "/images/003.jpg", alt: "Myphoto" },
+    { src: "/images/004.jpg", alt: "Myphoto" },
   ]
 
   return (
