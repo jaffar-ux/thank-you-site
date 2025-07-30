e1d6a9c1a49c935a013198498f0fbcf4bd5da022
jaffar-ux <hawlaadmi@gmail.com>
2025-07-31 00:02:03 +0530

    Update AudioPlayer.js

--- src/components/AudioPlayer.js
+++ src/components/AudioPlayer.js
@@ -1,20 +1,20 @@
 'use client';
 
-import { useRef, useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
 export default function AudioPlayer() {
   const audioRef = useRef(null);
 
   useEffect(() => {
     if (audioRef.current) {
-      audioRef.current.volume = 0.5;
+      audioRef.current.volume = 2;
     }
   }, []);
 
   return (
-    <div>
+    <div style={{ padding: '1rem', textAlign: 'center' }}>
       <audio ref={audioRef} controls>
-        <source src="/audio/paro.mp3" type="audio/mpeg" />
+        <source src="/public/paro.mp3" type="audio/mpeg" />
         Your browser does not support the audio element.
       </audio>
     </div>
