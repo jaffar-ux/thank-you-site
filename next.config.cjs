c2e98a68b1878bf4f8ed574ebea82be9b8c1ffc4
jaffar-ux <hawlaadmi@gmail.com>
2025-07-30 16:42:10 +0530

    Update next.config.cjs

--- next.config.cjs
+++ next.config.cjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
