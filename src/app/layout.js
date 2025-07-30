f27fd6a703f4810edff42b20a3565fc0d049e8f0
jaffar-ux <hawlaadmi@gmail.com>
2025-07-30 23:56:18 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,9 @@
-'use client';
-
-import "./globals.css";
-import AudioPlayer from "../components/AudioPlayer";
+import './globals.css';
+import AudioPlayer from '../components/AudioPlayer';
 
 export const metadata = {
-  title: "I Have Something to Say...",
-  description: "This isn't just a website. It's something truly special.",
+  title: 'I Have Something to Say...',
+  description: "This isn't just a website. It's something deeper.",
 };
 
 export default function RootLayout({ children }) {
